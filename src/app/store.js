4fa7e93814e9be712720995fe89ee5a851dbbab4
JoelAngels <joelbaraka4325@gmail.com>
2021-05-01 19:02:07 +0300

    made the movie reducer page in redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
+import movieReducer from "../features/movie/movieSlice";
 
 //setting the redux store
 export default configureStore({
   reducer: {
     user: userReducer,
-    // movie: movieReducer,
+    movie: movieReducer,
   },
   middleware: getDefaultMiddleware({
     serializableCheck: false,
