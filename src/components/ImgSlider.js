7ac6ad379adbd46633fac3a2e8f6304010e45235
JoelAngels <joelbaraka4325@gmail.com>
2021-05-01 12:44:48 +0300

    rendered out the imgslider

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ImgSlider = (props) => {
+  return (
+    <div>
+      <h1>Creating the </h1>
+      <h1>Creating the Image Slider</h1>
+    </div>
+  );
+};
+
+export default ImgSlider;
