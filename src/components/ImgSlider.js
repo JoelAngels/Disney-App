d6581e2a1832cbc7af7c2da2ef8595d617a8d960
JoelAngels <joelbaraka4325@gmail.com>
2021-05-01 12:59:46 +0300

    set the slider functionality

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -1,11 +1,29 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ImgSlider = (props) => {
+  let settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
   return (
-    <div>
-      <h1>Creating the </h1>
-      <h1>Creating the Image Slider</h1>
-    </div>
+    <Slider {...settings}>
+      <div>
+        <h3>1</h3>
+      </div>
+      <div>
+        <h3>2</h3>
+      </div>
+      <div>
+        <h3>3</h3>
+      </div>
+    </Slider>
   );
 };
 
